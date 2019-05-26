8236df80e0f3faef8bf36f03d3c68f68614ea436
Dogend233 <dogend@xn--o5raa.com>
2019-05-26 21:17:32 +0800

    2

---
+++ mchub/mchub.js
@@ -0,0 +1,53 @@
+jQuery(document).ready(function( $ ) {
+
+  // Hero rotating texts
+  $("#main-frame .rotating").Morphext({
+    animation: "rotateInUpLeft",
+    separator: ",",
+    speed: 3000
+  });
+  
+  // Initiate the wowjs
+  new WOW().init();
+  
+  // Stick the header at top on scroll
+  $("#header").sticky({topSpacing:0, zIndex: '50'});
+  
+  // Smoth scroll on page hash links
+  $('a[href*="#"]:not([href="#"])').on('click', function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          if (target.length) {
+              
+              var top_space = 0;
+              
+              if( $('#header').length ) {
+                top_space = $('#header').outerHeight();
+              }
+              
+              $('html, body').animate({
+                  scrollTop: target.offset().top - top_space
+              }, 1500, 'easeInOutExpo');
+              
+              return false;
+          }
+      }
+  });
+  
+  // Back to top button
+  $(window).scroll(function() {
+
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+      
+  });
+  
+  $('.back-to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+      return false;
+  });
+
+});
